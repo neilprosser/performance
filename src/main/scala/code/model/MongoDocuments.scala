0b8144070de55e60acacf34b3b380ecd0e8e82aa
Neil Prosser <neil.prosser@gmail.com>
2011-03-30 20:11:28 +0100

    Tweaked stuff to hopefully make things a touch simpler.

--- src/main/scala/code/model/MongoDocuments.scala
+++ src/main/scala/code/model/MongoDocuments.scala
@@ -10,42 +10,9 @@ class RunDoc private () extends MongoRecord[RunDoc] with MongoId[RunDoc] {
 	
 	object name extends StringField(this, 128)
 	object description extends StringField(this, 32)
-	object tests extends DBRefField[RunDoc, TestDoc](this, TestDoc)
 	object timestamp extends DateTimeField(this)
 	
 }
 
 object RunDoc extends RunDoc with MongoMetaRecord[RunDoc] {
-}
-
-class TestDoc private () extends MongoRecord[TestDoc] with MongoId[TestDoc] {
-	
-	def meta = TestDoc
-	
-	object name extends StringField(this, 128)
-	
-}
-
-object TestDoc extends TestDoc with MongoMetaRecord[TestDoc] {
-}
-
-class SampleDoc private () extends MongoRecord[SampleDoc] with MongoId[SampleDoc] {
-	
-	def meta = SampleDoc
-	
-}
-
-object SampleDoc extends SampleDoc with MongoMetaRecord[SampleDoc] {
-}
-
-class PropertyDoc private () extends MongoRecord[PropertyDoc] with MongoId[PropertyDoc] {
-	
-	def meta = PropertyDoc
-	
-	object name extends StringField(this, 32)
-	object value extends StringField(this, 256)
-	
-}
-
-object PropertyDoc extends PropertyDoc with MongoMetaRecord[PropertyDoc] {
 }
--- src/main/scala/code/snippet/PerformanceRest.scala
+++ src/main/scala/code/snippet/PerformanceRest.scala
@@ -1,35 +1,30 @@
-package code {
-package snippet {
+package code.snippet
 
 import net.liftweb.http._
 import net.liftweb.http.rest._
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.JsonDSL._
 import net.liftweb.json.JsonAST._
+
+import com.mongodb._
 
 import code.model._
 
-import org.bson.types._
-
 object PerformanceRest extends RestHelper {
 
-  serve {
-	  
-    case Req("runs" :: id :: Nil, "json", GetRequest) => {
-    	JObject(RunDoc.find(id.toString).map(r => JField("name", JString(r.name.toString))).toList)
-    }
-    
-    case Req("runs" :: Nil, "json", PostRequest) => {
-    	val run = RunDoc.createRecord
-    	run.save
-    	JObject(JField("id", JString(run.id.toString)) :: Nil)
-    }
-    
-    case Req("tests" :: name, "json", GetRequest) => {
-    	JObject(Nil)
-    }
-    
-  }
+	serve {
+		
+		case "runs" :: id :: _ JsonGet _ => {
+			RunDoc.find(id)
+			JObject(Nil)
+		}
+		
+		case "runs" :: _ JsonPut _ => {
+			val run = RunDoc.createRecord
+			run.save
+			JObject(JField("id", run.id.toString) :: Nil)
+		}
+		
+	}
   
-}
-
-}
 }
