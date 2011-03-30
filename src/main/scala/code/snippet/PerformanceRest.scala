90c1d76f813908403d1c9c9c988e29cc55c4a5e3
Neil Prosser <neil.prosser@gmail.com>
2011-03-31 00:11:21 +0100

    Reading and writing work. Error handling is pretty much non-existent when writing!

--- src/main/scala/code/snippet/PerformanceRest.scala
+++ src/main/scala/code/snippet/PerformanceRest.scala
@@ -1,28 +1,39 @@
 package code.snippet
 
+import net.liftweb.common._
+import net.liftweb.util.Helpers._
 import net.liftweb.http._
 import net.liftweb.http.rest._
-import net.liftweb.json.DefaultFormats
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json.JsonAST._
-
-import com.mongodb._
 
 import code.model._
 
 object PerformanceRest extends RestHelper {
-
+	
 	serve {
 		
-		case "runs" :: id :: _ JsonGet _ => {
-			RunDoc.find(id)
-			JObject(Nil)
+		case "runs" :: id :: Nil JsonGet _ => {
+			for {
+				r <- RunDoc.find(id) ?~ "Run not found"
+			} yield r.asJSON
 		}
 		
-		case "runs" :: _ JsonPut _ => {
-			val run = RunDoc.createRecord
-			run.save
-			JObject(JField("id", run.id.toString) :: Nil)
+		case "runs" :: name :: description :: Nil JsonGet _ => {
+			for {
+				r <- RunDoc.find(("name" -> name)~("description" -> description)) ?~ "Run not found"
+			} yield r.asJSON
+		}
+		
+		case "runs" :: Nil JsonPut json -> _ => {
+			
+			val run = for {
+				JField("name", JString(name)) <- json
+				JField("description", JString(description)) <- json
+			} yield RunDoc.createRecord.name(name).description(description)
+			
+			run.head.save
+			run.head.asJSON
 		}
 		
 	}
