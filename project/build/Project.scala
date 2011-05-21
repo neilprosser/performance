c50434c6e93809e02f5522a2adfca42016a377bb
Neil Prosser <neil.prosser@gmail.com>
2011-05-21 11:06:35 +0100

    Untidy business

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -28,7 +28,8 @@ class Project(info: ProjectInfo) extends DefaultWebProject(info) with AkkaProjec
   override val akkaActor  = akkaModule("actor") withSources() // it's good to always have the sources around
   val akkaHttp            = akkaModule("http")  withSources()
   val spray               = "cc.spray" %% "spray" % "0.5.0" % "compile" withSources()
-  val liftJson           = "net.liftweb" %% "lift-json" % "2.3" % "compile" withSources()
+  val liftJson            = "net.liftweb" %% "lift-json" % "2.3" % "compile" withSources()
+  val casbah              = "com.mongodb.casbah" %% "casbah" % "2.1.2" % "compile"
 
   val JETTY_VERSION = "8.0.0.M2"
   val specs       = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
